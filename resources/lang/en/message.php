ca2c8bcb6a0692e39d4cdc56352b66b3e09a1d7c
Jetulio <joo_ramos2006@hotmail.com>
2026-04-28 11:53:08 -0300

    chore: add custom message configuration

---
+++ resources/lang/en/message.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'products' => [
+        'name_unchangeable' => 'Product name cannot be changed',
+        'name_required' => 'Name must not be empty.',
+        'name_not_found' => 'Product with name :name not found.',
+        'name_unique' => 'Product with name :name already exists',
+        'min_name_length' => 'Name must be at least :min characters long.',
+        'max_name_length' => 'Name must not exceed :max characters',
+        'max_description_length' => 'Description must not exceed :max characters',
+        'amount_required' => 'Amount must be greater than 0 (ZERO).',
+        'quantity_required' => 'Quantity must be equals or greater than 0 (ZERO).',
+        'quantity_exceed' => 'Not enough :name in stock. Only :quantity available',
+        'cannot_delete_qty' => 'Cannot delete product with quantity greater than 0',
+    ],
+    'general' => [
+        'fbd_op' => 'Forbidden operation',
+    ],
+];
